8d0c9ad0f95daaecc1fb3f62c0112d2c1df5daf1
batuhand <batuhandemircann@gmail.com>
2022-06-23 19:14:28 +0300

    file select dialog and read selectedfile into list of string

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
 	"time"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/container"
 	"fyne.io/fyne/dialog"
+	"fyne.io/fyne/storage"
 	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
 )
@@ -35,6 +38,42 @@ func scanTap(win fyne.Window, progress *widget.ProgressBar) {
 
 }
 
+func attackerScreen(win fyne.Window) fyne.CanvasObject {
+	btn := widget.NewButton("Open .txt files", func() {
+		// Using dialogs to open files
+		// first argument func(fyne.URIReadCloser, error)
+		// 2nd is parent window in our case "w"
+		// r for reader
+		// _ is ignore error
+		file_Dialog := dialog.NewFileOpen(
+			func(r fyne.URIReadCloser, _ error) {
+				// read files
+
+				data, _ := ioutil.ReadAll(r)
+				//print(string(data))
+				testlist := strings.Split(string(data), "\n")
+				//print(testlist)
+				for i := 0; i < len(testlist); i++ {
+					print(testlist[i])
+				}
+
+			}, win)
+		// fiter to open .txt files only
+		// array/slice of strings/extensions
+		file_Dialog.SetFilter(
+			storage.NewExtensionFileFilter([]string{".txt"}))
+		file_Dialog.Show()
+		// Show file selection dialog.
+	})
+
+	attackScreen := container.NewCenter(container.NewVBox(
+		widget.NewLabel("Attacker"),
+		btn,
+	))
+
+	return attackScreen
+}
+
 func scannerScreen(win fyne.Window) fyne.CanvasObject {
 	progress := widget.NewProgressBar()
 	scanScreen := container.NewCenter(container.NewVBox(
@@ -52,7 +91,7 @@ func main() {
 	tabs := container.NewAppTabs(
 		container.NewTabItemWithIcon("Home", theme.HomeIcon(), homeScreen()),
 		container.NewTabItemWithIcon("Scanner", theme.SearchIcon(), scannerScreen(myWindow)),
-		container.NewTabItemWithIcon("Attacker", theme.ComputerIcon(), widget.NewLabel("Attacker tab")),
+		container.NewTabItemWithIcon("Attacker", theme.ComputerIcon(), attackerScreen(myWindow)),
 		container.NewTabItemWithIcon("Settings", theme.SettingsIcon(), widget.NewLabel("Settings tab")),
 	)
 
